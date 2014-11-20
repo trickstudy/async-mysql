0db7f49ce8e38098e40dfdcc6fb7be9c35fcf377
James Hulett <git@fpgr.org>
2014-11-20 15:18:06 -0500

    Comments and streamlines

--- src/db_drivers/AsyncMysql.php
+++ src/db_drivers/AsyncMysql.php
@@ -28,37 +28,39 @@ class AsyncMysql {
    * @return React\Promise\PromiseInterface
    */
   function query($query) {
-    return $this->pool->getConnection()->then(function ($conn) use ($query) {
-      $status = $conn->query($query, MYSQLI_ASYNC);
-      if ($status === false) {
-        $this->pool->freeConnection($conn);
-        throw new \Exception($conn->error);
+    return $this->pool->getConnection()->then(function ($conn) use ($query) {  // getConnection returns a promise, so we use then to process it
+      $status = $conn->query($query, MYSQLI_ASYNC);  // Send the query to MySQL specifying async
+      if ($status === false) {  // Means the query failed
+        $this->pool->freeConnection($conn);  // Release the connection back to the pool
+        throw new \Exception($conn->error);  // Throw a PHP Exception with the MySQL error
       }
 
-      $defered = new Deferred();
+      $defered = new Deferred();  // Create the deferred to return to our caller
 
-      $this->loop->addPeriodicTimer(0.001, function ($timer) use ($conn, $defered) {
-        $links = $errors = $reject = array($conn);
-        mysqli_poll($links, $errors, $reject, 0); // don't wait, just check
-        if (($read = in_array($conn, $links, true)) || ($err = in_array($conn, $errors, true)) || ($rej = in_array($conn, $reject, true))) {
-          if ($read) {
-            $result = $conn->reap_async_query();
-            if ($result === false) {
-              $defered->reject(new \Exception($conn->error));
-            } else {
-              $defered->resolve($result);
+      $this->loop->addPeriodicTimer(0.001, function ($timer) use ($conn, $defered) {  // Tell React to run the below every 0.001 seconds
+        $links = $errors = $reject = array($conn);  // Initialize some vars
+        mysqli_poll($links, $errors, $reject, 0);  // Poll for results, now, and at the top of every execution of this periodic function
+        if (($read = in_array($conn, $links, true))  // Our connection appearing in the $links array means we have results waiting 
+          || ($err = in_array($conn, $errors, true))  // Our connection appearing in the $error array means we have errors
+          || ($rej = in_array($conn, $reject, true))) {  // Our connection appearing in the $reject array means our query was rejected
+          if ($read) {  // If we have results to read
+            $result = $conn->reap_async_query();  // Load the query results from the connection with them waiting ($conn)
+            if ($result === false) {  // Means the provided results indicate a query error
+              $defered->reject(new \Exception($conn->error));  // Throw a PHP Excpetion with said error
+            } else {  // Otherwise we have "good" results
+              $defered->resolve($result);  // So resolve the deferred with the query results
             }
-          } elseif ($err) {
-            $defered->reject($conn->error);
-          } else {
-            $defered->reject(new \Exception('Query was rejected'));
+          } elseif ($err) {  // If we found errors in out polling...
+            $defered->reject($conn->error);  // reject the deferred with those errors
+          } else {  // In all other cases
+            $defered->reject(new \Exception('Query was rejected'));  // reject the deferred indicating the MySQL rejection
           }
-          $timer->cancel();
-          $this->pool->freeConnection($conn);
+          $timer->cancel();  // We have results, rejection, or errors to stop checking every 0.001
+          $this->pool->freeConnection($conn);  // Release the connection back into the pool
         }
       });
 
-      return $defered->promise();
+      return $defered->promise();  // Return our promise to the caller
     });
   }
 }
